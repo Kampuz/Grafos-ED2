61a8f3c7b6d91ef1e81800c2111251facaa343e7
Miguel de Campos Rodrigues Moret <miguel.moret@unesp.br>
2024-11-14 15:51:47 -0300

    feat: inplemented search by depth

---
+++ Busca em Profundidade/lista.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "../CodeBase/lista.h"
+
+int *encontraComponentes(PonteiroGrafo grafo) {
+    int s, c = 0, *componentes = (int*)malloc(grafo->numVertices * sizeof(int));
+
+    for (s = 0; s < grafo->numVertices; s++)
+        componentes[s] = -1;
+    for (s = 0; s < grafo->numVertices; s++)
+        if (componentes[s] == -1) {
+            visitaRec(grafo, componentes, c, s);
+            c++;
+        }
+    return componentes;
+}
+
+void visitaRec(PonteiroGrafo grafo, int *componentes, int componente, int vertice) {
+    ponteiroNO no;
+    componentes[vertice] = componente;
+    for (no = grafo->lista[vertice]; no != NULL; no->proximo)
+        if (componentes[no->vertice] == -1)
+            visitaRec(grafo, componentes, componente, no->vertice);
+}
+
+int *encontraCaminhos(PonteiroGrafo grafo, int s) {
+    int i, *pai = (int*)malloc(grafo->numVertices * sizeof(int));
+    for (i = 0; i < grafo->numVertices; i++)
+        pai[i] = -1;
+    buscaEmProfundidade(grafo, pai, s, s);
+    return pai;
+}
+
+void buscaEmProfundidade(PonteiroGrafo grafo, int *pai, int p, int vertice) {
+    ponteiroNO no;
+    pai[vertice] = p;
+    for (no = grafo->lista[vertice]; no != NULL; no->proximo)
+        if (pai[no->vertice] == -1)
+            buscaEmProfundidade(grafo, pai, vertice, no->vertice);
+}
+
+void imprimirCaminhoReverso(int vertice, int *pai) {
+    printf("%d", vertice);
+    if (pai[vertice] != vertice)
+        imprimirCaminhoReverso(pai[vertice], pai);
+}
+
+void imprimirCaminho(int vertice, int *pai) {
+    if (pai[vertice] != vertice)
+        imprimirCaminho(pai[vertice], pai);
+    printf("%d", vertice);
+}
--- Busca em Profundidade/matriz.c
+++ Busca em Profundidade/matriz.c
@@ -1,1 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 #include "../CodeBase/matriz.h"
+
+int existeCaminho(PonteiroGrafo grafo, int inicio, int fim) {
+    int encontrou, i, *visitado = (int*)malloc(grafo->numVertices * sizeof(int));
+
+    for (i = 0; i < grafo->numVertices; i++)
+        visitado[i] = 0;
+
+    encontrou = buscaRec(grafo, visitado, inicio, fim);
+    free(visitado);
+    return encontrou;
+}
+
+int buscaRec(PonteiroGrafo grafo, int *visitado, int chave, int fim) {
+    int proximo;
+    if(chave == fim)
+        return 1;
+    
+    visitado[chave] = 1;
+    for (proximo = 0; proximo < grafo->numVertices; proximo++)
+        if (grafo->matriz[chave][proximo] && !visitado[proximo])
+            if (buscaRec(grafo, visitado, proximo, fim))
+                return 1;
+
+    return 0;
+}
+
+int *encontrarCaminhos(PonteiroGrafo grafo, int inicio) {
+    int i;
+    int *pai = (int*)malloc(grafo->numVertices * sizeof(int));
+
+    for (i = 0; i < grafo->numVertices; i++)
+        pai[i] = -1;
+    buscaEmProfundidade(grafo, pai, inicio, inicio);
+    return pai;
+}
+
+void buscaEmProfundidade(PonteiroGrafo grafo, int *pai, int verticePai, int vertice) {
+    int proximo;
+    pai[vertice] = verticePai;
+
+    for (proximo = 0; proximo < grafo->numVertices; proximo++)
+        if (grafo->matriz[vertice][proximo] && pai[proximo] == -1)
+            buscaEmProfundidade(grafo, pai, vertice, proximo);
+}
+
+void imprimirCaminhoReverso(int vertice, int *pai) {
+    printf("%d", vertice);
+    if (pai[vertice] != vertice)
+        imprimirCaminhoReverso(pai[vertice], pai);
+}
+
+void imprimirCaminho(int vertice, int *pai) {
+    if (pai[vertice] != vertice)
+        imprimirCaminho(pai[vertice], pai);
+    printf("%d", vertice);
+}
